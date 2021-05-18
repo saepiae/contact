5a9ec75f0b27aa97db13fec07e7bc73d3b2cddc1
Saepiae <irinanr4@mail.ru>
2021-05-18 10:00:31 +0300

    task004: шаблон для домашней страницы

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет из Notebook"))
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Упс... что-то пошло не так", 500)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
