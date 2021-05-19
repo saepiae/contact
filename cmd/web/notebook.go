ace5dde923b1478cf3cb9b1b309a5d2ef47c886c
Saepiae <irinanr4@mail.ru>
2021-05-19 22:41:56 +0300

    task006: логирование с метками и в http

--- cmd/web/notebook.go
+++ cmd/web/notebook.go
@@ -4,11 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", ":8090", "Сетевой адрес HTTP")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.LstdFlags)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.LstdFlags|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", home)
 	mux.HandleFunc("/contact/all", allContacts)
@@ -18,7 +22,13 @@ func main() {
 	mux.HandleFunc("/contact/delete", deleteContact)
 	mux.HandleFunc("/contact/dublicates", findDublicatedContacts)
 
-	log.Printf("Запуск веб-сервера на %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Запуск веб-сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
